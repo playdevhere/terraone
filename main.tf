a18946098f9aca17ea7a1c7ba802d9c2d888007c
Himanshu <rathihimanshu@live.com>
2021-07-29 17:03:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "mymachine" {
-  count = terraform.workspace == "dev" ? 1 : 5
+  count = terraform.workspace == "terraone/dev" ? 1 : 5
   ami = var.ami
   instance_type = var.type
   key_name = "june2021"
