aa16976ef5f3c40a89181285dc366d7a5ff387ba
abcd <himanshu@springpeople.com>
2021-07-29 16:58:28 +0530

    second

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "mymachine" {
   count = terraform.workspace == "dev" ? 1 : 5
-  ami = "ami-0c2b8ca1dad447f8a"
-  instance_type = "t2.micro"
+  ami = var.ami
+  instance_type = var.type
   key_name = "june2021"
   tags = {
-    Name = "name1"
+    Name = var.vmname
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "usernames" {
   type    = list(any)
   default = ["one", "two", "three", "four"]
 }
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami" {}
+variable "type" {}
+variable "vmname" {}
